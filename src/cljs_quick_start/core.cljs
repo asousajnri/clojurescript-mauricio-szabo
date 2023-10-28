a2a5425c791d445923ec50809ba1998123299fde
Antônio <a.sousa.jnri@gmail.com>
2023-10-28 11:12:50 -0300

    First routes

--- src/cljs_quick_start/core.cljs
+++ src/cljs_quick_start/core.cljs
@@ -1,6 +1,22 @@
 (ns cljs-quick-start.core
-  (:require react-dom))
+  (:require ["express" :as express]))
 
-(.render js/ReactDOM
-         (.createElement js/React "h2" nil "Hello, Hello!")
-         (.getElementById js/document "app"))
+(def app (express))
+(def port 3000)
+
+(defn handle-root [req res]
+  (. res send "Hello, the whole world!"))
+
+(defn register-handlers []
+  (. app get "/"
+    (fn [req res] (handle-root req res)))
+
+  (. app listen port (fn []
+                       (println "Example app listenning on port" port))))
+
+(defn ^:dev/after-load main []
+  (def server (register-handlers))
+
+ (defn ^:dev/before-load stop-server []
+   (. server close)
+   (println "Stopped server")))
